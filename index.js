1434fbfea62a89142d85c57aafbd70e90fde0eb3
Esteban Jiménez <esteban.jimenez@mercadolibre.com.co>
2023-03-22 12:11:41 -0500

    Subiendo funciones

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 //realizar clases segun las operaciones
+let a = prompt("");
+let b = prompt("");
+
+function suma(a, b) {
+    return a + b;
+}
+function resta(a, b) {
+    return a - b;
+}
+function multiplicacion(a, b) {
+    return a * b;
+}
+function division(a, b){
+    return a / b;
+}
 //prueba
